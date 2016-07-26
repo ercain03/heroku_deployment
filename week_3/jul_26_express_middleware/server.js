4507c0faa5fe207bdde1d868e076f5458f11fd94
Tyler Morgan <toasty@Tylers-MacBook-Pro.local>
2016-07-26 14:45:09 -0700

    convoluted example of error stuff in middleware

--- week_3/jul_26_express_middleware/server.js
+++ week_3/jul_26_express_middleware/server.js
@@ -3,14 +3,37 @@ const debug = require('debug');
 const serverLog = debug('cfdemo:serverlog');
 const serverError = debug('cfdemo:servererror');
 
-app.get('/middleware', (req, res, next) => {
-  serverLog('hello from middleware');
+app.use((req, res, next) => {
+  serverLog('from a .use');
   next();
-}, (req, res) => {
-  res.json({msg: 'end of middleware'});
+});
+
+app.post('/middleware', (req, res, next) => {
+  next(new Error('error from .post'));
+});
+
+app.get('/middleware', (req, res, next) => {
+  serverLog('inside another .get');
+  next(new Error('error from another .get'));
+});
+
+app.get('/middleware', (err, req, res, next) => {
+  serverLog('one more middleware: ' + err.message);
+  next();
+});
+
+app.get('/middleware', (req, res, next) => {
+  next(new Error('some random error'));
 }, (err, req, res, next) => {
-  serverError('in request to ' + req.url + ' using method: ' + req.method);
-  serverError(err);
+  serverError('first middleware: ' + err.message);
+  next();
+}, (req, res, next) => {
+  res.send('error processed\n');
+});
+
+app.use((err, req, res, next) => {
+  serverError('from the use: ' + err.message);
+  next();
 });
 
 app.listen(3000, () => console.log('server up'));
