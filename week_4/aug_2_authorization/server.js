771e39585aeb344509d458930500ef61551c19e1
toastynerd <tyler@codefellows.com>
2016-08-02 12:59:57 -0700

    start of class

---
+++ week_4/aug_2_authorization/server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+if (!process.env.APP_SECRET) throw new Error('Please set the env APP_SECRET');
+const mongoose = require('mongoose');
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost/auth_dev');
+let app = require('express')();
+let authRouter = require('./routes/auth_router');
+let serverError = require('debug')('cfdemo:error');
+
+app.use('/api', authRouter);
+
+app.use((err, req, res, next) => {
+  serverError(err);
+  res.status(err.statusCode || 500).json(err.message);
+});
+app.listen(3000, () => console.log('server up'));
